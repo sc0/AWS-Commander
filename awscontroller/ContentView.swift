1a5a00d108180ca71da9b19c0420c1184fef1953
Kacper Debowski <kdebowski93@gmail.com>
2020-10-21 23:04:39 +0200

    Initial Commit

---
+++ awscontroller/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  awscontroller
+//
+//  Created by Kacper Debowski on 21/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
